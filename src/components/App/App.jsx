32025122c025533dbabfcf9f8d0546893a5b1c78
KSK86-Kim <70407461+KSK86-Kim@users.noreply.github.com>
2021-10-03 13:09:45 +0300

    add example

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <>
       <h1 className={s.title}>Привет</h1>
+      <h1 className={s.title_test}>Привет</h1>
       <button type="button" className={s.title_btn}>
         asasdasd
       </button>
