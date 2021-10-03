d7efdda47fbdeeb9d17d1a5ce4f9b86f6a5dfe57
KSK86-Kim <70407461+KSK86-Kim@users.noreply.github.com>
2021-10-03 13:26:04 +0300

    add dynamic class

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,22 @@
 import s from "./App.module.scss";
-function App() {
+function App({ arr }) {
   return (
     <>
       <h1 className={s.title}>Привет</h1>
       <h1 className={s.title_test}>Привет</h1>
-      <button type="button" className={s.title_btn}>
+
+      <h2>Запросы</h2>
+      <ul>
+        {arr.map(({ test, type }) => (
+          <li key={type}>
+            <p className={s[type]}>{test}</p>
+          </li>
+        ))}
+      </ul>
+
+      {/* <button type="button" className={s.title_btn}>
         asasdasd
-      </button>
+      </button> */}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom";
 import "modern-normalize/modern-normalize.css";
 import App from "./components/App";
 
+const arr = [
+  {
+    test: "все хорошо",
+    type: "success",
+  },
+  {
+    test: "кто-то в себе в ногу выстрел",
+    type: "error",
+  },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App arr={arr} />
   </React.StrictMode>,
   document.getElementById("root")
 );
