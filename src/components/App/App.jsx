e2b73449cec270d81eefd8cb14f499762bd6b496
KSK86-Kim <70407461+KSK86-Kim@users.noreply.github.com>
2021-10-03 12:54:52 +0300

    fisrt

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import s from "./App.module.scss";
+function App() {
+  return (
+    <>
+      <h1 className={s.title}>Привет</h1>
+      <button type="button" className={s.title_btn}>
+        asasdasd
+      </button>
+    </>
+  );
+}
+
+export default App;
